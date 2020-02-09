cddea9589137cd32e45583948480a5765d67e259
Alfiado Ngana <alfiadoprojectos@gmail.com>
2020-02-09 23:25:55 +0200

    forgot password parte final restando a senha

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const crypto = require('crypto')
+const moment = require('moment')
 
 const User = use('App/Models/User')
 const Mail = use('Mail')
@@ -34,6 +35,34 @@ class ForgotPasswordController {
       return response
         .status(err.status)
         .send({ error: { message: 'Algo deu errado' } })
+    }
+  }
+
+  async update({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        response
+          .status(401)
+          .send({ error: { message: 'o Token esta expirado' } })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (error) {
+      response
+        .status(error.status)
+        .send({ error: { message: 'Algo deu errado ao resetar a senha' } })
     }
   }
 }
