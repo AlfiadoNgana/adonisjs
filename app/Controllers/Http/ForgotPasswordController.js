86a003d700a72cc9ec7798964ec0dfdfbcac723d
Alfiado Ngana <alfiadoprojectos@gmail.com>
2020-02-09 21:05:07 +0200

    forgotpassword parte 2 enviando email para recuperar a senha

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -3,6 +3,7 @@
 const crypto = require('crypto')
 
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store({ request, response }) {
@@ -14,6 +15,21 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`,
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('alfiado@ngana.com')
+            .subject('recuperacao de senha')
+        }
+      )
     } catch (err) {
       return response
         .status(err.status)
